8e712bf1c2d6db1dba03d4bef37cc85579457a65
MarkosVS <markos.vs@live.com>
2016-05-22 10:43:24 -0300

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,20 @@
+// Questão 02 - Processo Seletivo Acens
+/**
+* @author Marcos Vinicius dos Santos Sombra
+*/
+class Q2 {
+	public static void main (String[] args){
+		// inicio do 1º for
+		for(int i = 1; i <= 9; i++){
+			// inicio da estrutura condicional
+			if(i != 3){
+				// inicio do 2º for
+				for(int j = 1; j <= 6; j++){
+					// escreve "oi"
+					System.out.print("oi");
+				}
+			}
+		}
+	}
+}
+// esse código imprime a palavra "oi" 48 vezes
