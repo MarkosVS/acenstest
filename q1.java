8c08e18e3c41e496f714d28ee5ec36865482c513
MarkosVS <markos.vs@live.com>
2016-05-22 09:19:42 -0300

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,20 @@
+// Questão 01 - Processo Seletivo Acens
+/**
+* @author Marcos Vinicius dos Santos Sombra
+*/
+class Q1 {
+	public static void main (String[] args){
+		// declaração das variáveis x e y
+		int x = 2;
+		int y = 5;
+		// início da estrutura condicional
+		if(y > 8){
+			y = y * 2;
+		}
+		else{
+			x = x * 2;
+		}
+		// imprime na tela o resultado da soma das variaveis
+		System.out.print(x+y);
+	}
+}
