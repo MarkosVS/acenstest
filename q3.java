76a7b0b607374a11aa52e8007a922fcd4a7075d6
MarkosVS <markos.vs@live.com>
2016-05-23 08:54:08 -0300

    Update q3.java

--- q3.java
+++ q3.java
@@ -4,10 +4,14 @@
 */
 class Q3 {
 	public static void main (String[] args){
+		// populações iniciais
 		double a = 80000;
 		double b = 200000;
+		// contador de anos
 		int cont = 1;
+		// inicio do while
 		while(a < b){
+			// taxas de crescimento anuais
 			double cresA = a * 0.03;
 			double cresB = b * 0.015;
 			a = a + cresA;
@@ -15,6 +19,7 @@ class Q3 {
 			System.out.println("Após o " + cont + "º ano, a população de A era " + a + " e a de B era " + b + ".");
 			cont++;
 		}
+		// imprime na tela quantos anos leva para atingir a condição
 		System.out.println("Levou " + --cont + " anos para a população de A ultrapassar/igualar a de B.");
 	}
 }
