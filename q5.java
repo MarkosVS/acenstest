76133c694a2b1e2b59e3681a33f76e591944310e
MarkosVS <markos.vs@live.com>
2016-05-27 10:42:56 -0300

    Add files via upload

---
+++ q5.java
@@ -0,0 +1,76 @@
+﻿// Questão 05 - Processo Seletivo Acens
+/**
+* @author Marcos Vinicius dos Santos Sombra
+*/
+
+class Q5 {
+	public static void main (String[] args){
+		// declaracao e preenchimento do Array de numeros
+		String[] lista = new String[60];
+		lista[0] = "213752";
+		lista[1] = "216732";
+		lista[2] = "221063";
+		lista[3] = "221545";
+		lista[4] = "225583";
+		lista[5] = "229133";
+		lista[6] = "230648";
+		lista[7] = "233222";
+		lista[8] = "236043";
+		lista[9] = "237330";
+		lista[10] = "239636";
+		lista[11] = "240138";
+		lista[12] = "242123";
+		lista[13] = "246224";
+		lista[14] = "249183";
+		lista[15] = "252936";
+		lista[16] = "254711";
+		lista[17] = "257200";
+		lista[18] = "257607";
+		lista[19] = "261424";
+		lista[20] = "263814";
+		lista[22] = "266794";
+		lista[23] = "268649";
+		lista[24] = "273050";
+		lista[25] = "275001";
+		lista[26] = "277606";
+		lista[27] = "278997";
+		lista[28] = "287104";
+		lista[29] = "287953";
+		lista[30] = "289137";
+		lista[31] = "291591";
+		lista[32] = "292559";
+		lista[33] = "292946";
+		lista[34] = "295180";
+		lista[35] = "295566";
+		lista[36] = "297529";
+		lista[37] = "300400";
+		lista[38] = "304707";
+		lista[39] = "306931";
+		lista[40] = "310638";
+		lista[41] = "313595";
+		lista[42] = "318449";
+		lista[43] = "319021";
+		lista[44] = "322082";
+		lista[45] = "323796";
+		lista[46] = "326266";
+		lista[47] = "326880";
+		lista[48] = "327249";
+		lista[49] = "329914";
+		lista[50] = "334392";
+		lista[51] = "334575";
+		lista[52] = "336723";
+		lista[53] = "336734";
+		lista[54] = "338808";
+		lista[55] = "343269";
+		lista[56] = "346040";
+		lista[57] = "350113";
+		lista[58] = "353631";
+		lista[59] = "357154";
+		
+		// declaração do Array de números possíveis
+		String[] listaPossivel = new String[60];
+		for(int i = 0; i <= lista.length; i++){
+			System.out.print(lista[i].charAt(0));	
+		}
+	}		
+}
