63ff39f8383b50f26ddd46e62d26a1bf7c87cfe5
MarkosVS <markos.vs@live.com>
2016-05-27 15:48:09 -0300

    Update q6.java

--- q6.java
+++ q6.java
@@ -5,5 +5,6 @@
 class Q6 {
 	public static void main (String[] args){
 		// nao tenho ideia de como desenvolver um crawler
+		// pesquisei na internet mas ainda assim não consegui compreender bem o processo
 	}
 }
